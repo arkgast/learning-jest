a3327b3203e1e27b4995a7b0a1d8038aff012aff
Arnold Gandarillas Castillo <arkgast@gmail.com>
2017-09-24 19:27:41 -0400

    Add App component tests
    Ensures that CommentBox and CommentList are part of App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import CommentBox from './CommentBox'
+import CommentList from './CommentList'
 
 
 export default () => (
   <div>
     <h1>React rocks!</h1>
     <CommentBox />
+    <CommentList list={[]} />
   </div>
 )
--- test/components/App.test.js
+++ test/components/App.test.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import App from '../../src/components/App'
+import CommentBox from '../../src/components/CommentBox'
+import CommentList from '../../src/components/CommentList'
 
 
 describe('App', () => {
-  it('renders correctly', () => {
-    shallow(<App />)
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallow(<App />)
   })
 
-  it('has a h1 tag', () => {
-    const wrapper = shallow(<App />)
+  it('shows a h1 tag', () => {
     expect(wrapper.length).toEqual(1)
+  })
+
+  it('shows a CommentBox component', () => {
+    expect(wrapper.contains(<CommentBox />)).toBeTruthy()
+  })
+
+  it('shows a CommentList component', () => {
+    expect(wrapper.contains(<CommentList list={[]} />)).toBeTruthy()
   })
 })
