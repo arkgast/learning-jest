4e9f58d199f72f06721b94c40de23c7bdde019ad
Arnold Gandarillas Castillo <arkgast@gmail.com>
2017-09-26 13:01:12 -0400

    Fix CommentList tests
    Pass fake data as a props not as a state because it's a dump component.
    It's parent Comment is the container that passes the information to
    CommentList and CommentBox

--- test/components/CommentList.test.js
+++ test/components/CommentList.test.js
@@ -5,14 +5,14 @@ import CommentList from '../../src/components/CommentList'
 describe('CommentList', () => {
   it('shows all comments', () => {
     const comments = [ 'New comment', 'Another comment' ]
-    const wrapper = renderComponent(CommentList, {comments})
+    const wrapper = renderComponent(CommentList, {}, {comments})
     comments.forEach((comment, i) => {
       expect(wrapper.find(`li[data-id='${i}']`).text()).toEqual(comment)
     })
   })
 
   it('shows a message if the list is empty', () => {
-    const wrapper = renderComponent(CommentList, {comments: []})
+    const wrapper = renderComponent(CommentList, {}, {comments: []})
     expect(wrapper.find('li').length).toEqual(1)
     expect(wrapper.find('li').text()).toEqual('There is no comments yet')
   })
