c2525b409059e49316c65d3369e38b245e351d90
Arnold Gandarillas Castillo <arkgast@gmail.com>
2017-09-26 12:58:58 -0400

    Fix Comment test
    Check if user is authenticated

--- test/components/Comment.test.js
+++ test/components/Comment.test.js
@@ -7,15 +7,32 @@ import CommentList from '../../src/components/CommentList'
 
 
 describe('<Comment />', () => {
-  let wrapper
-  beforeEach(() => {
-    wrapper = renderComponent(Comment)
-  })
-  it('shows a CommentBox component', () => {
-    expect(wrapper.contains(<CommentBox />)).toBeTruthy()
+  describe('User is authenticated', () => {
+    let wrapper
+    beforeEach(() => {
+      wrapper = renderComponent(Comment, { authenticated: true })
+    })
+    it('shows a CommentBox component', () => {
+      expect(wrapper.contains(<CommentBox />)).toBeTruthy()
+    })
+
+    it('shows a CommentList component', () => {
+      expect(wrapper.contains(<CommentList comments={[]} />)).toBeTruthy()
+    })
   })
 
-  it('shows a CommentList component', () => {
-    expect(wrapper.contains(<CommentList comments={[]} />)).toBeTruthy()
+  /**
+   * If the user is not authenticated the function push will be called
+   */
+  describe('User is not authenticated', () => {
+    it('redirects to the home page', () => {
+      const history = {
+        push: () => {}
+      }
+      const spy = jest.spyOn(history, 'push')
+
+      renderComponent(Comment, {}, { history })
+      expect(spy).toHaveBeenCalled()
+    })
   })
 })
